476edac15aeccece592f57fd27ac814c88dc469d
herdal06 <h.erdal721@gmail.com>
2022-07-17 17:45:26 +0300

    Initial commit

---
+++ app/src/main/java/com/example/buylist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.buylist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
